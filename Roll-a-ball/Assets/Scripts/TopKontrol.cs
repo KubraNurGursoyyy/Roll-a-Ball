4bb2a66ab21d1a4e52972826ee67297834aa7dc3
Kübra Nur Gürsoy <55765380+KubraNurGursoyyy@users.noreply.github.com>
2023-08-21 14:01:40 +0300

    Roll-a-Ball

---
+++ Roll-a-ball/Assets/Scripts/TopKontrol.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TopKontrol : MonoBehaviour
+{
+    public int hiz;
+    public Rigidbody fizik;
+    public int puan;
+    public int objeSayisi;
+    public Text puanText;
+    public Text oyunBittiText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fizik = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //if (Input.GetKeyDown(KeyCode.Escape))
+        //{
+        //    Debug.Log("Esc");
+        //}
+
+        float yatay = Input.GetAxis("Horizontal");
+        float dikey = Input.GetAxis("Vertical");
+
+        Vector3 vector = new Vector3(yatay, 0, dikey);
+
+        fizik.AddForce(vector * hiz);
+
+    }
+
+    //bir çarpışma olduğu anda bu fonksiyon tetiklenir
+    void OnTriggerEnter(Collider other)//collider çarpışma demek
+    {
+        //buradaki other çarpışma yaşanan şey oluyor
+        //Destroy(other.gameObject);//bu other game object diyoruz bu şekilde
+        other.gameObject.SetActive(false);
+        puan++;
+
+        puanText.text = "Puan : " + puan;
+
+        if(puan == objeSayisi)
+        {
+            oyunBittiText.gameObject.SetActive(true);
+        }
+    
+    }
+
+}
